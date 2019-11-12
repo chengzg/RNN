b416d42be4dfe25bd74f32e1170868bc8461f95f
chengzhigao <chengzhigao@virtuosgames.com>
2019-11-12 10:07:09 +0800

    first commit

---
+++ ClassifyNames.py
@@ -0,0 +1,35 @@
+from __future__ import unicode_literals, print_function, division
+from io import open
+import glob, os
+import unicodedata
+import string
+
+all_letters = string.ascii_letters + " .,;'"
+n_letters = len(all_letters)
+
+def findFiles(path): return glob.glob(path)
+
+def unicodeToAscii(s):
+    return ''.join(
+        c for c in unicodedata.normalize('NFD', s)
+        if unicodedata.category(c) != 'Mn'
+        and c in all_letters
+    )
+
+print(unicodeToAscii('Ślusàrski'))
+
+category_lines = {}
+all_categories = []
+
+def readLines(filename):
+    lines = open(filename, encoding='utf-8').read().strip().split('\n')
+    return [unicodeToAscii(line) for line in lines]
+
+for filename in findFiles('data/names/*.txt'):
+    category = os.path.splitext(os.path.basename(filename))[0]
+    all_categories.append(category)
+    lines = readLines(filename)
+    category_lines[category] = lines
+
+n_categories = len(all_categories)
+print("number of categories: ", n_categories)
